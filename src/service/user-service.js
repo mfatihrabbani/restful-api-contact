d9b88cbe607fc7f24e1598191bcc6df15b71b6d7
mfatihrabbani <mfatihrabbani@gmail.com>
2023-06-25 00:48:58 +0700

    user register api

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,8 +1,33 @@
+import { prismaClient } from "../application/database.js"
+import { ResponseError } from "../error/response-error.js"
 import { registerUserValidation } from "../validation/user-validation.js"
 import { validate } from "../validation/validation.js"
+import bcrypt from "bcrypt"
 
-const register = (request) => {
+const register = async (request) => {
     const user = validate(registerUserValidation, request)
 
-    
+    const countUser = await prismaClient.user.count({
+        where: {
+            username: user.username
+        }
+    })
+
+    if(countUser === 1){
+        throw new ResponseError(400, "Username already exist")
+    }
+
+    user.password = await bcrypt.hash(user.password, 10)
+
+    return prismaClient.user.create({
+        data: user,
+        select: {
+            username: true,
+            name: true,
+        }
+    })
+}
+
+export default {
+    register
 }
