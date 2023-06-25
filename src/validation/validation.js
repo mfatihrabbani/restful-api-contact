18ae625e39a2fa191d9ee5661d5626a93bae7ae0
mfatihrabbani <mfatihrabbani@gmail.com>
2023-06-25 08:40:52 +0700

    login user api

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -3,6 +3,7 @@ import { ResponseError } from "../error/response-error.js"
 const validate = (schema, request) => {
     const result = schema.validate(request, {
         abortEarly: false,
+        allowUnknown: false
     })
     if (result.error){
         throw new ResponseError(400, result.error.message)
